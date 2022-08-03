e1b9e5bc919172bd36890a8ec06d3c5bae8c9a99
Panupong Rong-O <tayalone@gmail.com>
2022-08-03 22:13:39 +0700

    🎉 สร้าง apollo client

---
+++ libs/apollo-client.ts
@@ -0,0 +1,8 @@
+import { ApolloClient, InMemoryCache } from '@apollo/client'
+
+const apolloClient = new ApolloClient({
+  uri: 'https://api-ap-northeast-1.hygraph.com/v2/cl6d7s88w00nu01ueebp02myw/master',
+  cache: new InMemoryCache(),
+})
+
+export default apolloClient
