41eded12957ed7864aabe06a04ec9b0689eb5109
Panupong Rong-O <tayalone@gmail.com>
2022-08-03 21:47:42 +0700

    🔧 เพิ่มให้ auto import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
-import type { AppProps } from 'next/app'
+import type { AppProps, NextWebVitalsMetric } from 'next/app'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
+}
+
+export function reportWebVitals(metric: NextWebVitalsMetric) {
+  console.info(metric)
 }
 
 export default MyApp
