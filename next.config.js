0983d4f389c2af19126ab48b1e88dc415040cacd
Panupong Rong-O <tayalone@gmail.com>
2022-08-10 20:42:19 +0700

    ✨ set env GRAPH_QL_URI

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  serverRuntimeConfig: {
+    // Will only be available on the server side
+    graphQlUri: process.env.GRAPH_QL_URI,
+  },
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+  },
   reactStrictMode: true,
   swcMinify: true,
   images: {
